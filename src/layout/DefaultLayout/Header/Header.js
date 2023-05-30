f16743e41607d298ddf74ba42932d23022890152
DuocDoSoS <A35742@thanglong.edu.vn>
2023-05-30 23:51:47 +0700

    update code

--- src/layout/DefaultLayout/Header/Header.js
+++ src/layout/DefaultLayout/Header/Header.js
@@ -9,25 +9,49 @@ const categories = [
     {
         id: 1,
         name: "Jewelry",
-        option: [
-
-        ]
+        options: [
+            {
+                kind: "Nhẫn",
+                option: [
+                        "nhẫn bạc",
+                        "nhẫn vàng",
+                        "nhẫn kim loại"
+                ]
+            },
+            {
+                kind: "dây chuyền",
+                option: [
+                        "dây chuyền bạc",
+                        "dây chuyền vàng",
+                        "dây chuyền kim loại"
+                ]
+            },
+            {
+                kind: "khuyên tai",
+                option: [
+                        "dây chuyền bạc",
+                        "dây chuyền vàng",
+                        "dây chuyền kim loại"
+                ]
+            }
+        ],
     },
     {
         id: 2,
         name: "Sale",
-        option: [
-            
-        ]
     },
     {
         id: 3,
         name: "Gift",
-        option: [
-            
+        select: [
+            "quà tặng cho nam",
+            "quà tặng cho nữ",
+            "quà tặng cho nữ",
+            "quà tặng cho nữ"
         ]
     }
 ]
+
 function Header() {
     return <header className={cx("wrapper")}>
         <div className={cx("content")}>
@@ -36,11 +60,39 @@ function Header() {
             </div>
             <div className={cx("categories")}>
                 {categories.map((category, index)=>{
+                    let OptionElement = <div></div>
+                    let SelectElement = <div></div>
+                    if(category.options !== undefined){
+                        OptionElement = <div className={cx("wrapperTable")}>
+                                            <div className={cx("table")}>
+                                                {category.options.map((option, index)=> {
+                                                    return <div key={index} className={cx("item")}>
+                                                                <div className={cx("title")}>{option.kind}</div>
+                                                                {option.option.map((value, key)=> {
+                                                                    return <div key={key} className={cx("material")}>{value}</div>
+                                                                })}
+                                                            </div>
+                                                })}
+                                            </div>
+                                        </div>
+                    }
+                    if(category.select !== undefined){
+                        SelectElement = <div className={cx("columns")}>
+                            {category.select.map((value, key) => {
+                                return <div key={key} className={cx("column")}>
+                                    {value}
+                                </div>
+                            })}
+                        </div>
+                    }
                     return <div key={index} className={cx("category")}>
-                        {category.name}
-                    </div>
+                                    {category.name}
+                                    {OptionElement}
+                                    {SelectElement}
+                                </div>
                 })}
             </div>
+    
             <div className={cx("cart")}>
                 <div className={cx("search")}>
                     <FontAwesomeIcon className={cx("searchIcon")} icon={faMagnifyingGlass}/>
